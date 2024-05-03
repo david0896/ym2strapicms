0a7e987deffc5e582328030ee852a62f906ef2bc
david0896 <davidvasquez0896@gmail.com>
2024-05-03 19:18:54 -0400

    config email plugin

--- config/plugins.js
+++ config/plugins.js
@@ -1,1 +1,25 @@
-module.exports = () => ({});
+module.exports = ({ env }) => ({
+    'users-permissions': {
+      config: {
+        jwtSecret: env('JWT_SECRET'),
+      },
+    },
+    email: {
+      config: {
+        provider: 'nodemailer',
+        providerOptions: {
+          service: "hotmail",
+          auth: {
+            user: "qtequenoswebapp@outlook.com",
+            pass: "bnjolnbcizconrfz"
+          },
+          // ... any custom nodemailer options
+        },
+        settings: {
+          defaultFrom: 'qtequenoswebapp@outlook.com',
+          defaultReplyTo: 'qtequenoswebapp@outlook.com',
+          testAddress: 'qtequenoswebapp@outlook.com',
+        },
+      },
+    },
+  });
